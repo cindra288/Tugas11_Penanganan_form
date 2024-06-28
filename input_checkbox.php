3895447add7cbb62783cc96c6b2ea53643527f36
cindra288 <cindramokoagow6@gmail.com>
2024-06-29 01:51:13 +0700

    Add files via upload

---
+++ input_checkbox.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Grup Band Favorit</title>
+</head>
+<body>
+	<form action="prosescheckbox.php" method="post" name="input">
+		<h2>Pilih Grub Band yang menjadi favoritmu</h2>
+		<input type="checkbox" name="band1" value="Kahitna" checked> Kahitna <br>
+		<input type="checkbox" name="band2" value="Tipe-X"> Tipe-X <br>
+		<input type="checkbox" name="band3" value="Yovie & Nuno"> Yovie & Nuno <br>
+		<input type="checkbox" name="band4" value="Superman Is Dead"> Superman Is Dead <br>
+		<input type="submit" name="Pilih" value="Pilih">
+		<input type="reset" name="reset" value="Reset">
+	</form>
+</body>
+</html>
